09a4e2edd29487a7a7324b3f9e6c20589069e750
ouo5487ouo <97513508+ouo5487ouo@users.noreply.github.com>
2023-03-11 11:48:44 +0800

    [DC.js-3]指令架構

--- src/commands/ping/ping.js
+++
@@ -1,9 +0,0 @@
-import { SlashCommandAssertions, SlashCommandBuilder } from "discord.js";
-
-export const command = new SlashCommandBuilder()
-    .setName('ping')
-    .setDescription('ping command')
-
-export const action = async(ctx) => {
-    ctx.reply('pong!')
-}
--- src/main.js
+++ src/main.js
@@ -1,14 +1,19 @@
-import { Client, Events, GatewayIntentBits } from 'discord.js';
-import dotenv from 'dotenv';
+import { Client, Events, GatewayIntentBits } from 'discord.js'
+import vueInit from '@/core/vue'
+import dotenv from 'dotenv'
+import { useAppStore } from '@/store/app'
 
+import { loadCommands, loadEvents } from '@/core/loader'
+
+vueInit()
 dotenv.config()
 
-const client = new Client({ intents: [GatewayIntentBits.Guilds] });
+loadCommands()
 
+const client = new Client({ intents: [GatewayIntentBits.Guilds] })
+const appStore = useAppStore()
+appStore.client = client
 
-client.once(Events.ClientReady, c => {
-	console.log(`Ready! Logged in as ${c.user.tag}`);
-});
+loadEvents()
 
-
-client.login(process.env.TOKEN);
+client.login(process.env.TOKEN)
